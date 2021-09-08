e7bab767f18ee8e0ab95dfee8447031ae91e8944
Ben Hodges <benjaminshodges@gmail.com>
2021-09-08 10:14:26 -0400

    First iteration of minimax method, currentely not working.

--- src/TTTPVSC.java
+++ src/TTTPVSC.java
@@ -1,24 +1,32 @@
 import java.util.List;
 
-public class TTTPVSC extends TTTPVDC implements Cloneable {
-
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
+public class TTTPVSC extends TTTPVDC {
 
     private boolean firstMove = true;
 
-    private void copyBoardFromInstance(TTTBase prevInstance) {
-        board = prevInstance.board;
+    private TTTPVSC copyBoardFromInstance(TTTBase prevInstance) {
+        TTTPVSC copiedGame = new TTTPVSC();
+        copiedGame.board = prevInstance.board;
+        copiedGame.currentPlayer = prevInstance.currentPlayer;
+        return copiedGame;
     }
 
     @Override
     protected void handleComputerMove() {
         if (firstMove) {
             board[0][0] = currentPlayer;
-            return;
+            firstMove = false;
         } else {
             MinimaxReturn result = minimax(this, true, 8);
+            TTTPVSC chosenGame = result.getBoard();
+            chosenGame.displayBoard();
+            for (int row = 0; row < 3; row++) {
+                for (int col = 0; col < 3; col++) {
+                    if (board[row][col] != chosenGame.board[row][col]) {
+                        board[row][col] = Player.X;
+                    }
+                }
+            }
         }
     }
 
@@ -32,7 +40,6 @@ public class TTTPVSC extends TTTPVDC implements Cloneable {
         } return 0;
     }
 
-
     private MinimaxReturn minimax(TTTPVSC game, boolean isMax, int depth) {
         if (depth == 0 || checkForWin() || checkForTie()) {
             return new MinimaxReturn(game.staticEval(), game);
@@ -41,24 +48,37 @@ public class TTTPVSC extends TTTPVDC implements Cloneable {
         List<int[]> childPositions = findOpenPositions();
 
         if (isMax) {
-            TTTPVSC bestGame;
+            TTTPVSC bestGame = this;
             int maxEval = -10000;
             for (int[] childPosition: childPositions) {
                 board[childPosition[0]][childPosition[1]] = Player.X;
                 MinimaxReturn returnValues = minimax(this, false, depth - 1);
                 int eval = returnValues.getEval();
                 if (eval >= maxEval) {
+                    bestGame = copyBoardFromInstance(this);
                     maxEval = eval;
                 }
-
+                game.board[childPosition[0]][childPosition[1]] = Player.NONE;
             }
-        } else {
-            TTTPVSC bestGame;
-            int minEval = 10000;
+            return new MinimaxReturn(maxEval, bestGame);
         }
 
+        else {
+            TTTPVSC bestGame = this;
+            int minEval = 10000;
+            for (int[] childPosition: childPositions) {
+                board[childPosition[0]][childPosition[1]] = Player.O;
+                MinimaxReturn returnValues = minimax(this, false, depth - 1);
+                int eval = returnValues.getEval();
+                if (eval <= minEval) {
+                    bestGame = copyBoardFromInstance(this);
+                    minEval = eval;
+                }
+                game.board[childPosition[0]][childPosition[1]] = Player.NONE;
+            }
+            return new MinimaxReturn(minEval, bestGame);
+        }
 
-        return new MinimaxReturn(10, this);
     }
 
 }
